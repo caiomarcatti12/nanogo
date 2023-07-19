00e0ea5aba82bbf14b0aa19a52f8a814d7199687
Caio Marcatti <caiomarcatti12@gmail.com>
2023-07-19 14:04:32 -0300

    Adicionado uma forma de incrementar as rotas

--- .examples/start_webserver.go
+++ .examples/start_webserver.go
@@ -9,6 +9,14 @@ func main() {
 	// Carrega o arquivo .env
 	env.LoadEnv()
 
-	// Inicializa o webserver
-	webserver.StartWebServer()
+	server := webserver.NewWebServer()
+
+	//caso desejar utilizar outras rotas
+	//runRouter := config.WebServerRouter()
+	//server.AddRouter("/run", runRouter)
+	//
+	//otherRouter := outras_rotas.OutrasRotasRouter()
+	//server.AddRouter("/", otherRouter)
+
+	server.Start()
 }
--- config/webserver/webserver.go
+++ config/webserver/webserver.go
@@ -6,14 +6,31 @@ import (
 	"os"
 
 	"github.com/codelesshub/nanogo/config/log"
+	"github.com/gorilla/mux"
 )
 
-func StartWebServer() {
+type WebServer struct {
+	router *mux.Router
+}
+
+func NewWebServer() *WebServer {
+	return &WebServer{
+		router: mux.NewRouter(),
+	}
+}
+
+func (ws *WebServer) AddRouter(path string, router *mux.Router) {
+	ws.router.PathPrefix(path).Handler(router)
+}
+
+func (ws *WebServer) Start() {
 	port := getPortWebServer()
+
+	ws.AddRouter("/", WebServerRouter())
 
 	fmt.Printf("Servidor iniciado em localhost:%s\n", port)
 
-	log.Fatal(http.ListenAndServe(":"+port, WebServerRouter()))
+	log.Fatal(http.ListenAndServe(":"+port, ws.router))
 }
 
 func getPortWebServer() string {
