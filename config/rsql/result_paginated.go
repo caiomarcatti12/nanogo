c5f9882a50ecedb8376c4dee5f25c736a8925542
Caio Marcatti <caiomarcatti12@gmail.com>
2023-09-19 20:28:05 -0300

    Adiciona parser dos parametros passado por url

--- config/rsql/result_paginated.go
+++ config/rsql/result_paginated.go
@@ -1,8 +1,6 @@
 package rsql
 
-import "github.com/caiomarcatti12/nanogo/v2/config/repository"
-
-type ResultPaginated[T repository.Model] struct {
+type ResultPaginated[T interface{}] struct {
 	Rows  []T   `json:"rows"`
 	Total int64 `json:"total"`
 }
