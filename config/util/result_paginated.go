f5a7b99cef3225b2ba3aab50c3de89bed1b661eb
Caio Marcatti <caiomarcatti12@gmail.com>
2023-09-19 22:01:38 -0300

    Adiciona parser dos parametros passado por url

--- config/util/result_paginated.go
+++ config/util/result_paginated.go
@@ -1,6 +1,6 @@
 package util
 
-type ResultPaginated struct {
-	Rows  []interface{} `json:"rows"`
-	Total int64         `json:"total"`
+type ResultPaginated[T any] struct {
+	Rows  T     `json:"rows"`
+	Total int64 `json:"total"`
 }
