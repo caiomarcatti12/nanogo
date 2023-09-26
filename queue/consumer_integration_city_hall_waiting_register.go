6fa4b65eda7ab84e5d8761258b248f3ddb6cbdb7
Caio Marcatti <caiomarcatti12@gmail.com>
2023-09-25 21:33:40 -0300

    Remove arquivos de teste

--- queue/consumer_integration_city_hall_waiting_register.go
+++
@@ -1,8 +0,0 @@
-package queue
-
-import "log"
-
-func ConsumerIntegrationCityHallWaitingRegister(body map[string]interface{}, headers map[string]interface{}) {
-	log.Printf("Headers: %v", headers)
-	log.Printf("Body: %s", body)
-}
