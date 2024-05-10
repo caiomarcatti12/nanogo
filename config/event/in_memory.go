7fc0ad700afb999b048ee1ccbf71664cb09691db
Caio Marcatti <caiomarcatti12@gmail.com>
2024-05-10 14:03:28 -0300

    Alteração do evento para possuir mais que um handler

--- config/event/in_memory.go
+++ config/event/in_memory.go
@@ -9,24 +9,27 @@ import (
 type EventHandler func(Event)
 
 type InMemoryBroker struct {
-	handlers map[string]EventHandler
+	handlers map[string][]EventHandler // Mudança para um slice de EventHandler
 	logger   log.ILog
 }
 
 func NewInMemoryBroker(logger log.ILog) IEventDispatcher {
 	return &InMemoryBroker{
-		handlers: make(map[string]EventHandler),
+		handlers: make(map[string][]EventHandler),
 		logger:   logger,
 	}
 }
 
 func (b *InMemoryBroker) RegisterConsumer(eventKey string, handler EventHandler) {
-	b.handlers[eventKey] = handler
+	// Adiciona o handler ao slice para o eventKey específico
+	b.handlers[eventKey] = append(b.handlers[eventKey], handler)
 }
 
 func (b *InMemoryBroker) Dispatch(event Event) {
-	if handler, exists := b.handlers[event.Key]; exists {
-		handler(event)
+	if handlers, exists := b.handlers[event.Key]; exists {
+		for _, handler := range handlers {
+			go handler(event) // Executa cada handler em uma nova goroutine
+		}
 	} else {
 		b.logger.Warning(fmt.Sprintf("Nenhum manipulador encontrado para o evento: %s", event.Key))
 	}
