46afe67f92d1505792eea325c65c585296f6f165
Caio Marcatti <caiomarcatti12@gmail.com>
2025-05-09 01:49:39 -0300

    Add utility functions for directory handling
    Introduce `GetCurrentDir` and `GetAbsolutePath` to simplify directory retrieval and path resolution. These functions enhance code maintainability and are essential for managing file paths efficiently.

---
+++ pkg/util/directory.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Caio Matheus Marcatti Calimério
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package util
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+func GetCurrentDir() string {
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("Não foi possível obter diretório atual")
+	}
+	return filepath.Dir(filename)
+}
+
+func GetAbsolutePath(path string) string {
+	// Sobe dois níveis a partir do diretório atual
+	currentDir := GetCurrentDir()
+	rootDir := filepath.Join(currentDir, "..", "..")
+	absRootDir, err := filepath.Abs(rootDir)
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(absRootDir, path)
+}
