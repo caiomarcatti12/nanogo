2c04ada98c5f1e35eedd9cede4aeb11b389d7a99
Caio Marcatti <caiomarcatti12@gmail.com>
2025-05-09 19:07:37 -0300

    Remove unused import and comment out debug logging
    The unused "fmt" import was removed to clean up the code. Additionally, the debug log for loading env files was commented out to reduce unnecessary logging in production.

--- pkg/env/env_file_loader.go
+++ pkg/env/env_file_loader.go
@@ -16,7 +16,6 @@
 package env
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -45,7 +44,7 @@ func (f *FileEnvLoader) Load() error {
 	}
 
 	for _, path := range possiblePaths {
-		log.Println(fmt.Sprintf("Loading env file: %s", path))
+		//log.Println(fmt.Sprintf("Loading env file: %s", path))
 		if _, err := os.Stat(path); err == nil {
 			if err := godotenv.Load(path); err != nil {
 				log.Fatalf(f.i18n.Get("env.load_error", map[string]interface{}{"path": path, "error": err}))
